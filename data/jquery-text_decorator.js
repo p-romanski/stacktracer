d61f241ccdafdf3c00a24bcf705784c59185bfbb
Piotr Romański <piotrek.romanski@gmail.com>
2014-08-05 00:21:35 +0200

    RegExp FIX: RegExp with "g" should not be used for simple match multiple times (http://stackoverflow.com/questions/2141974/javascript-regex-literal-with-g-used-multiple-times)

--- data/jquery-text_decorator.js
+++ data/jquery-text_decorator.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
-    jQuery.fn.decorateText = function (regex, decorator) {
+    jQuery.fn.decorateText = function (regexString, decorator) {
+        var matchRegex = new RegExp(regexString, "i");
+        var replaceRegex = new RegExp(regexString, "gi");
+
         return this.each(function () {
             $(this).contents().filter(function () {
-                return this.nodeType == 3 && regex.test(this.nodeValue);
+                return this.nodeType == 3 && matchRegex.test(this.nodeValue);
             }).replaceWith(function () {
-                return (this.nodeValue || "").replace(regex, function (match) {
+                return (this.nodeValue || "").replace(replaceRegex, function (match) {
                     return decorator(match);
                 });
             });
