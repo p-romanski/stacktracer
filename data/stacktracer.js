d00bb0eac66b1ac1df20d151e1b7f5d6f78899f0
Piotr Romański <piotrek.romanski@gmail.com>
2014-08-05 00:29:33 +0200

    Better javaFileRegex

--- data/stacktracer.js
+++ data/stacktracer.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     var spanDecorator = function (match) {
         return '<span class="stack-tracer">' + match + '</span>';
     };
-    var javaFileRegex = "[a-z]+[.]java[:][0-9]+";
+    var javaFileRegex = "[a-z0-9_]+[.]java[:][0-9]+";
 
     $("*").decorateText(javaFileRegex, spanDecorator);
     $("span.stack-tracer").click(function (e) {
