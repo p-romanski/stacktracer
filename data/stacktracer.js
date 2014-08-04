b8bb2ed596922b386c6025668f4e3367ed8724ac
Piotr Romański <piotrek.romanski@gmail.com>
2014-08-04 23:09:42 +0200

    Regex refactoring

--- data/stacktracer.js
+++ data/stacktracer.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     var stackSpanDecorator = function (match) {
         return '<span class="stack-tracer">' + match + '</span>';
     };
-    var javaFileRegex = new RegExp("[a-z]+\.java:[0-9]+", "gi");
+    var javaFileRegex = new RegExp("[a-z]+[.]java:[0-9]+", "gi");
 
     $("*").decorateText(javaFileRegex, stackSpanDecorator);
     $("span.stack-tracer").click(function (e) {
