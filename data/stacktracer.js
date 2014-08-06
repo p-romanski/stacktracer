a1b1a0f0e44c6265fc03e45096fbf9ea34f24318
Piotr Romański <piotrek.romanski@gmail.com>
2014-08-06 21:22:34 +0200

    Simple prefs for domain and file patterns

--- data/stacktracer.js
+++ data/stacktracer.js
@@ -1,11 +1,15 @@
-$(document).ready(function () {
+var runTracer = function (javaFilePattern) {
     var spanDecorator = function (match) {
         return '<span class="stack-tracer">' + match + '</span>';
     };
-    var javaFileRegex = "[a-z0-9_]+[.]java[:][0-9]+";
 
-    $("*").decorateText(javaFileRegex, spanDecorator);
+    $("*").decorateText(javaFilePattern, spanDecorator);
     $("span.stack-tracer").click(function (e) {
         self.port.emit("clicked", $(this).text());
     });
+};
+
+self.port.on('filePatternSet', function (filePattern) {
+    runTracer(filePattern);
 });
+
